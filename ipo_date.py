dee8043a82465a25cac985e7151dafe6aedabe2f
Wang Zhicheng <wangzhicheng.thu@gmail.com>
2015-08-20 13:47:08 +0800

    update_database

---
+++ ipo_date.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-08-13 08:31:27
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import pandas as pd
+import tushare as ts
+import mysql.connector
+import datetime
+import tushare_mysql
+
+mysqlEngine = tushare_mysql.toggleMysql()
+cursor = mysqlEngine.cursor(buffered = True)
+cursor.execute("use {};".format(tushare_mysql.DB_NAME))
+
+stmts = '''
+	SELECT CODE, TIMETOMARKET FROM `CHINASTOCK`.`STOCK_BASICS`;
+	'''
+cursor.execute(stmts)
+for pair in cursor:
+	# print "symbol: {}\tIPO: {}".format(*pair)
+	symbol = pair[0]
+	date_int = pair[1]
+	date_str = str(date_int)
+	date_tuple = (int(date_str[0:4]), int(date_str[4:6]), int(date_str[6:]))
+	ipo = datetime.date(*date_tuple)
+	
+	df = tushare_mysql.getHistKlineDf(symbol = symbol
+									   beginDate = str(ipo),
+									   endDate = '2012-8-15',
+									   retry_count = 5,
+									   pause = .01)
+	tushare_mysql.insertTableFromIPODate(symbol, df, mysqlEngine)
+
+	
---
+++ update_database.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-08-13 08:31:27
+# @Author  : Wang Zhicheng (wzc_jd@126.com)
+# @Version : $Id$
+
+import pandas as pd
+import tushare as ts
+import mysql.connector
+import datetime
+import tushare_mysql
+
+model_table = 'sz000001'
+Engine = tushare_mysql.togglemysql()
+symbols = tushare_mysql.symbolList(Engine)
+
+cursor = Engine.cursor(buffered=True)
+cursor.execute("use {};".format(tushare_mysql.DB_NAME))
+
+sql = "SELECT DATE FROM `{}`.`{}` ORDER BY DATE DESC LIMIT 1;".format(tushare_mysql.DB_NAME, model_table)
+cursor.execute(sql)
+
+date = cursor.fetchall()[0][0]
+date_str = str(date)
+date_tuple = date_str.split('-')
+date_tuple = [int(part) for part in date_tuple]
+date_tuple[2] += 1
+
+next_date = datetime.date(*date_tuple)
+next_date = str(next_date)
+today = datetime.date.today()
+today = str(today)
+
+sql_insert = '''
+	INSERT INTO `{}` (`DATE`, `OPEN`, `HIGH`, `CLOSE`, `LOW`, `VOLUME`, `TURNOVER`)
+	VALUES (%s, %s, %s, %s, %s, %s, %s);
+	'''
+
+for each in symbols:
+	symbol_df = tushare_mysql.getHistKlineDf(symbol=each,
+									  beginDate=next_date,
+									  endDate=today,
+									  retry_count=5)
+
+	symbol_df[1]['volume']=symbol_df[1]['volume']*100
+	table = tushare_mysql.tblNmae(each)
+	cursor.executemany(sql_insert.format(table), symbol_df[1])
+	Engine.commit()
+
+tushare_mysql.togglemysql()
